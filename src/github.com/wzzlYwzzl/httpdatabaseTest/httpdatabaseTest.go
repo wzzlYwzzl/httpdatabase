35f72985f24438d9df72d5914bbf3b2a2652633b
wzzlYwzzl <zjw123bit@126.com>
2016-06-16 10:08:19 +0800

    change gitignore

---
+++ src/github.com/wzzlYwzzl/httpdatabaseTest/httpdatabaseTest.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	. "github.com/wzzlYwzzl/httpdatabase/mysql"
+)
+
+func TestInsert() {
+	user := User{"test1", "test1"}
+	db, err := sql.Open("mysql", "root:123456@/zjw")
+	if err != nil {
+		panic("mysql open error")
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic("mysql connect is wrong")
+	}
+
+	err = Insert(db, user)
+	if err != nil {
+		panic("mysql insert error")
+	}
+}
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	TestInsert()
+}
