e5f693429839c866c4a0b7064a33422c770e3122
wzzlYwzzl <zjw123bit@126.com>
2016-07-05 15:38:17 +0800

    add app deployment resource management

--- build/createtable.sql
+++ build/createtable.sql
@@ -33,3 +33,15 @@ CREATE TABLE `resource` (
     UNIQUE KEY (`user_id`)
     FOREIGN KEY (`user_id`) REFERENCES userpasswd(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `deployment`;
+CREATE TABLE `deployment` (
+    `deploy_id` int NOT NULL AUTO_INCREMENT,
+    `user_id` int NOT NULL,
+    `app_name` varchar(100) NOT NULL,
+    `cpus_use` int DEFAULT NULL,
+    `mem_use` int DEFAULT NULL,
+    PRIMARY KEY (`deploy_id`),
+    UNIQUE KEY (`user_id`)
+    FOREIGN KEY (`user_id`) REFERENCES userpasswd(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/github.com/wzzlYwzzl/httpdatabase/client/client.go
+++ src/github.com/wzzlYwzzl/httpdatabase/client/client.go
@@ -211,3 +211,48 @@ func (c HttpDBClient) GetAllUserInfo() (*user.UserList, error) {
 
 	return userlist, nil
 }
+
+func (c HttpDBClient) AddApp(deploy *user.UserDeploy) (bool, error) {
+	url := "http://" + c.Host + "/api/v1/user/app"
+
+	bs, err := json.Marshal(*deploy)
+	if err != nil {
+		log.Println(err)
+		return false, err
+	}
+
+	reader := bytes.NewReader(bs)
+	resp, err := http.Post(url, "application/json", reader)
+	if err != nil {
+		log.Println(err)
+		return false, err
+	}
+
+	if resp.StatusCode == http.StatusCreated {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (c HttpDBClient) DeleteUser(appName string) error {
+	client := &http.Client{}
+	url := "http://" + c.Host + "/api/v1/user/app" + appName
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Println(err)
+		return false, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return false, err
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		return true, nil
+	}
+
+	return false, nil
+}
